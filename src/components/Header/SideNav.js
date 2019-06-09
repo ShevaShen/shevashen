220a0a4ba664ef76dd4c868864eb11048092cc71
ShevaShen <sheva7zj@hotmail.com>
2019-06-08 19:47:20 -0700

    added drawer to nav

---
+++ src/components/Header/SideNav.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: 'auto'
+  }
+});
+
+const SideNav = () => {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const classes = useStyles();
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <React.Fragment>
+      <Button onClick={toggleDrawer('right', true)}>
+        <MenuIcon />
+      </Button>
+      <SwipeableDrawer
+        anchor="right"
+        open={state.right}
+        onClose={toggleDrawer('right', false)}
+        onOpen={toggleDrawer('right', true)}
+      >
+        {sideList('right')}
+      </SwipeableDrawer>
+    </React.Fragment>
+  );
+};
+
+export default SideNav;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,43 +6,46 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import Hidden from '@material-ui/core/Hidden';
 import Search from '@material-ui/icons/Search';
 import atoms from '../atoms';
+import SideNav from './SideNav';
 
-const { Divider, Toolbar, Icon } = atoms;
+const { Divider, Toolbar } = atoms;
 
-const Header = () => (
-  <AppBar position="sticky" color="default" elevation={0}>
-    <Toolbar narrow>
-      <Grid container alignItems="center">
-        <Grid item xs>
-          <Grid container alignItems="center">
-            Sheva Shen
-            <Divider vertical />
-            UI/UX focused Front-end Developer
-          </Grid>
-        </Grid>
-        <Hidden xsDown>
+const Header = () => {
+  return (
+    <AppBar position="sticky" color="default" elevation={0}>
+      <Toolbar narrow>
+        <Grid container alignItems="center">
           <Grid item xs>
-            <TextField
-              variant="outlined"
-              placeholder="Search"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <Search />
-                  </InputAdornment>
-                )
-              }}
-            />
+            <Grid container alignItems="center">
+              Sheva Shen
+              <Divider vertical />
+              UI/UX focused Front-end Developer
+            </Grid>
           </Grid>
-        </Hidden>
-        <Grid item>
-          <Grid container justify="flex-end">
-            nav links here
+          <Hidden xsDown>
+            <Grid item xs>
+              <TextField
+                variant="outlined"
+                placeholder="Search"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Search />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+          </Hidden>
+          <Grid item>
+            <Grid container justify="flex-end">
+              <SideNav />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Toolbar>
-  </AppBar>
-);
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
